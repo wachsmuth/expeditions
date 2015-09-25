d96fcf65203d6555b48b6641b6019ba0beef58ce
Emil istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2015-09-25 20:16:43 +0200

    update inputLoop

--- expeditions/src/gamestate/Turn.java
+++ expeditions/src/gamestate/Turn.java
@@ -2,6 +2,7 @@ package gamestate;
 
 import java.util.ArrayList;
 import helpers.CardType;
+import helpers.InputLoop;
 
 public class Turn {
 
@@ -13,17 +14,22 @@ public class Turn {
 		if (Game.getRations() == 0){
 			
 		}
-		//change rations
+		//change rations at end of turn
 		Game.changeRations(-1);
 	}
 	
+	/*
+	 * Method that allows playing actions until no more can be played
+	 */
 	public static void playActions(){
 		while (Game.getActions() > 0 && Game.getCardsOfTypeInHand(CardType.ACTION).size() > 0){
 			ArrayList<ExpeditionCard> possibleCards = Game.getCardsOfTypeInHand(CardType.ACTION);
 			ArrayList<String> options = new ArrayList<>();
 			for (ExpeditionCard ec : possibleCards){
-				
+				options.add(ec.getName());
 			}
+			options.add("End actions.");
+			InputLoop.inputLoop("Choose a card to play or end actions.", options);
 		}
 	}
 }
--- expeditions/src/helpers/InputLoop.java
+++ expeditions/src/helpers/InputLoop.java
@@ -1,13 +1,13 @@
 package helpers;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputLoop {
 
-    public static int inputLoop(String question, String... options) {
+    public static int inputLoop(String... options) {
         String answerString;
         Scanner inputScanner = new Scanner(System.in);
-        System.out.println(question);
         System.out.println("Valid input is: ");
         int j = 0;
         int[] noOfOptions = new int[options.length];
@@ -37,5 +37,37 @@ public class InputLoop {
         }
         return userInput;
     }
+    
+    public static <T> int inputLoop(String question, ArrayList<T> stuff){
+    	String[] arguments = new String[stuff.size()];
+    	arguments = stuff.toArray(arguments);
+    	System.out.println(question);
+    	return inputLoop(arguments);
+    }
+    /*
+    public static <T> T inputLoop(ArrayList<T> stuff) {
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Valid input is: ");
+        for (int i = 0; i < stuff.size(); i++) {
+            System.out.println(i + " " + stuff.get(i));
+        }
+        int userInput;
+        inputloop:
+        while (true) {
+            try {
+                System.out.print("Enter your input: ");
+                String userInputRaw = inputScanner.nextLine();
+                userInput = Integer.parseInt(userInputRaw);
+                //Check for extended commands
+                if (userInput < stuff.size() && userInput >= 0) {
+                    return stuff.get(userInput);
+                }
+                System.out.println("Not on the list of permitted input."); //If wrong number
+            } catch (NumberFormatException e) {
+                System.out.println("Input is not of correct type"); //this prints if 
+                //Do nothing. We keep trying.
+            }
+        }
+    }*/
 	
 }
